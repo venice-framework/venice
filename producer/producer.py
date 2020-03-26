ce29708b85dacb1559cd02245ac683ce5c9df58e
nantrinh <17187984+nantrinh@users.noreply.github.com>
2020-03-26 23:49:51 +0000

    cant connect to schema-registry... I suspect the python script ran too quickly and needs to wait for schema-registry to be ready

--- producer/producer.py
+++ producer/producer.py
@@ -8,35 +8,52 @@ SCHEMA_REGISTRY_URL = os.environ['SCHEMA_REGISTRY_URL']
 print(BROKER)
 print(SCHEMA_REGISTRY_URL)
 
-record_schema_str = avro.loads("""
-{
-   "namespace": "septa.bus.location",
-   "name": "value",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "lat",
-       "type" : "float",
-       "doc" : "latitude"
-     },
-     {
-       "name" : "lng",
-       "type" : "float",
-       "doc" : "longitude"
-     },
-     {
-       "name": "direction",
-       "type": "enum",
-       "doc": "the direction the bus is traveling",
-       "symbols": ["NorthBound", "SouthBound", "EastBound", "WestBound"]
-     },
-     {
-       "name": "route",
-       "type": "integer"
-     }
-   ]
-}
+value_schema = avro.loads("""
+    {
+        "namespace": "confluent.io.examples.serialization.avro",
+        "name": "User",
+        "type": "record",
+        "fields": [
+            {"name": "lat", "type": "float", "doc": "latitude"},
+            {"name": "lng", "type": "float", "doc": "longitude"}
+        ]
+    }
 """)
+
+value = {
+  "lat": 40.043152,
+  "lng": -75.18071
+}
+
+#record_schema_str = avro.loads("""
+#{
+#   "namespace": "septa.bus.location",
+#   "name": "Location",
+#   "type": "record",
+#   "fields" : [
+#     {
+#       "name" : "lat",
+#       "type" : "float",
+#       "doc" : "latitude"
+#     },
+#     {
+#       "name" : "lng",
+#       "type" : "float",
+#       "doc" : "longitude"
+#     },
+#     {
+#       "name" : "direction",
+#       "type" : "string",
+#       "doc" : "the direction the bus is traveling",
+#     },
+#     {
+#       "name" : "route",
+#       "type" : "integer"
+#       "doc" : "the bus route"
+#     }
+#   ]
+#}
+#""")
 
 #key_schema_str = """
 #{
@@ -62,20 +79,20 @@ record_schema_str = avro.loads("""
 #}
 #key = {"trip": 44802985}
 #
-#def delivery_report(err, msg):
-#    """ Called once for each message produced to indicate delivery result.
-#        Triggered by poll() or flush(). """
-#    if err is not None:
-#        print('Message delivery failed: {}'.format(err))
-#    else:
-#        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
-#
-#
-#avroProducer = AvroProducer({
-#    'bootstrap.servers': BROKER,
-#    'on_delivery': delivery_report,
-#    'schema.registry.url': SCHEMA_REGISTRY_URL 
-#    }, default_value_schema=value_schema)
-#
-#avroProducer.produce(topic='bus.locations', value=value)
-#avroProducer.flush()
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+avroProducer = AvroProducer({
+    'bootstrap.servers': BROKER,
+    'on_delivery': delivery_report,
+    'schema.registry.url': SCHEMA_REGISTRY_URL 
+    }, default_value_schema=value_schema)
+
+avroProducer.produce(topic='bus.locations', value=value)
+avroProducer.flush()
