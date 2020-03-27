b79fd0ea69793443410ee115087fe68184f2353e
nantrinh <17187984+nantrinh@users.noreply.github.com>
2020-03-27 04:30:33 +0000

    something is being written to the topic periodically. i will have to deserialize to check if it is what i intended.

--- producer/producer.py
+++ producer/producer.py
@@ -1,12 +1,26 @@
 import os
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
+import requests
+import time
+
+for i in range(120):
+  print("I am sleeping for {} seconds".format(i))
+  time.sleep(1)
+# time.sleep(20)
+print("I am done sleeping")
 
 BROKER = os.environ['BROKER']
 SCHEMA_REGISTRY_URL = os.environ['SCHEMA_REGISTRY_URL']
 
 print(BROKER)
 print(SCHEMA_REGISTRY_URL)
+
+r = requests.get(SCHEMA_REGISTRY_URL + '/subjects/')
+print("got my status code back! it's {}".format(r.status_code))
+while r.status_code != 200:
+    print("status code: {}. retrying...".format(r.status_code))
+    r = requests.get(SCHEMA_REGISTRY_URL + '/subjects/')
 
 value_schema = avro.loads("""
     {
@@ -20,10 +34,36 @@ value_schema = avro.loads("""
     }
 """)
 
-value = {
-  "lat": 40.043152,
-  "lng": -75.18071
-}
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+avroProducer = AvroProducer({
+    'bootstrap.servers': BROKER,
+    'on_delivery': delivery_report,
+    'schema.registry.url': SCHEMA_REGISTRY_URL 
+    }, default_value_schema=value_schema)
+
+lat = 40.043152
+lng = -75.18071
+
+while True:
+  for i in range(5):
+    value = {
+      "lat": lat,
+      "lng": lng 
+    }
+    avroProducer.produce(topic='bus.locations', value=value)
+    print("I just produced lat: {}, lng: {}".format(lat, lng))
+    lat += 0.000001
+    lng += 0.000001
+  avroProducer.flush()
+  print("I have flushed")
+  time.sleep(5)
 
 #record_schema_str = avro.loads("""
 #{
@@ -79,20 +119,7 @@ value = {
 #}
 #key = {"trip": 44802985}
 #
-def delivery_report(err, msg):
-    """ Called once for each message produced to indicate delivery result.
-        Triggered by poll() or flush(). """
-    if err is not None:
-        print('Message delivery failed: {}'.format(err))
-    else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
-
-
-avroProducer = AvroProducer({
-    'bootstrap.servers': BROKER,
-    'on_delivery': delivery_report,
-    'schema.registry.url': SCHEMA_REGISTRY_URL 
-    }, default_value_schema=value_schema)
-
-avroProducer.produce(topic='bus.locations', value=value)
-avroProducer.flush()
+#value = {
+#  "lat": 40.043152,
+#  "lng": -75.18071
+#}
